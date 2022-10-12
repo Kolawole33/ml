234de5e28d26f94ed261a28b5529deb7acd4d793
Kolawole33 <84532090+Kolawole33@users.noreply.github.com>
2022-10-12 15:19:06 +0100

    Update app.js

--- client/app.js
+++ client/app.js
@@ -25,8 +25,9 @@ function onClickedEstimatePrice() {
     var bathrooms = getBathValue();
     var location = document.getElementById("uiLocations");
     var estPrice = document.getElementById("uiEstimated");
-  
-   var url = "http://127.0.0.1:5000/predict_home_price";
+    
+    var url = "http://0.0.0.0:9696/predict_home_price";
+   //var url = "http://127.0.0.1:5000/predict_home_price";
    //var url = "/api/predict_home_price";
 
     $.post(url, {
@@ -45,7 +46,7 @@ function onClickedEstimatePrice() {
 function onPageLoad(){
 
     console.log("document loaded");
-    var url = "http://127.0.0.1:5000/get_location_names";
+    var url = "http://0.0.0.0:9696/get_location_names";
     //var url = "/api/get_location_names"; 
     $.get(url,function(data, status) {
         console.log("got response for get_location_names request");
